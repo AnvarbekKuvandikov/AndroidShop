357f3a41cf72e16069304c7681f213248e263cba
Loving <loving.uz0000@gmail.com>
2019-09-17 11:36:21 +0500

    Incoming products

--- app/src/main/java/com/example/shop/IncomingProducts.java
+++ app/src/main/java/com/example/shop/IncomingProducts.java
@@ -5,25 +5,40 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import java.util.Calendar;
 
 public class IncomingProducts extends AppCompatActivity {
     DatePickerDialog picker;
-    EditText eText;
+    EditText incomingDate;
+    EditText incomingNum;
+    AutoCompleteTextView incomingClient;
+    RadioButton incomingDollar;
+    Button incomingBack;
+    Button incomingSave;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.incomingproducts);
-        eText=(EditText) findViewById(R.id.incoming_date);
-        eText.setInputType(InputType.TYPE_NULL);
+        incomingDate=findViewById(R.id.incoming_date);
+        incomingDate.setInputType(InputType.TYPE_NULL);
+        incomingNum=findViewById(R.id.incoming_num);
+        incomingClient=findViewById(R.id.incoming_client);
+        incomingDollar=findViewById(R.id.incoming_dollar);
+        incomingBack=findViewById(R.id.incoming_back);
+        incomingSave=findViewById(R.id.incoming_save);
 
-        eText.setOnClickListener(new View.OnClickListener() {
+        incomingDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final Calendar cldr = Calendar.getInstance();
@@ -35,12 +50,14 @@ public class IncomingProducts extends AppCompatActivity {
                         new DatePickerDialog.OnDateSetListener() {
                             @Override
                             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                                eText.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                                incomingDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
                             }
                         }, year, month, day);
                 picker.show();
             }
         });
+
+
 
 
     }
